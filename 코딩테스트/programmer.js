31c9ca30e495a7b767ad26b8b9ab24af442db4d6
hnlim <77787731+limhaneul2244@users.noreply.github.com>
2023-07-05 23:51:27 +0900

    프로그래머스 lv1

---
+++ 코딩테스트/programmer.js
@@ -0,0 +1,41 @@
+//프로그래머스 1회차(230706)
+/**
+ * 같은 숫자는 싫어
+ */
+function solution (arr) {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] !== arr[i+1]) { //앞의 값과 뒤의 값을 비교
+            newArr.push(arr[i]);
+        }
+    }
+    console.log('newArr', newArr)
+    return newArr;
+}
+
+solution([1,1,3,3,0,1,1]);
+// solution([4,4,4,3,3]);
+// solution([4,4,4,3,3,2,2]);
+
+/**
+ * 3진법 뒤집기
+ * 3진법으로 변경
+ * 3진법에서 reverse
+ * reverse된 값을 10진법으로 다시 변경
+ */
+
+function reverse(num) {
+    let newStr = "";
+    let changeNum = num.toString(3);
+    console.log('3진법변경:', changeNum, typeof(changeNum));
+    for(let j = changeNum.length-1; j >= 0; j--) {
+        // newStr = newStr + changeNum[j];
+        newStr = `${newStr}${changeNum[j]}`;
+        console.log(newStr, typeof(newStr))
+    }
+    let newNum = parseInt(newStr, 3);
+    console.log(newNum)
+}
+
+
+reverse(45);
