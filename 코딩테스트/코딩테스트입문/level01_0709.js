6d8575ffb3a02a71050c415dfe979a014f6cfb07
임하늘 <hnlim2244@gmail.com>
2023-07-10 21:36:10 +0900

    프로그래머스 코딩테스트 lv0

--- 코딩테스트/코딩테스트입문/level01_0709.js
+++ 코딩테스트/코딩테스트입문/level01_0709.js
@@ -9,8 +9,8 @@ function solution2(numbers) {
     }
     return answer;
 }
-solution2([1,2,3,4,5]); //[2,4,6,8,10]
-solution2([1, 2, 100, -99, 1, 2, 3]); //[2, 4, 200, -198, 2, 4, 6]
+// solution2([1,2,3,4,5]); //[2,4,6,8,10]
+// solution2([1, 2, 100, -99, 1, 2, 3]); //[2, 4, 200, -198, 2, 4, 6]
 
 /**
  * 나머지구하기
@@ -21,18 +21,20 @@ function solution3 (num1, num2) {
     return answer;
 }
 
-solution3 (3, 2); //1
-solution3 (10, 5); //0
+// solution3 (3, 2); //1
+// solution3 (10, 5); //0
 
 /**
  * 중앙값 구하기
  */
-// 1. 오름차순 정렬하기
-// 2. 정렬 후 배열 중앙에 있는 값 찾기
 function solution4 (array) {
-    let answer = [];
-    array.sort()
-    console.log(array)
+    // 오름차순 정렬하기
+    const newArray = array.sort(function(a, b) {
+        return a-b;
+    })
+    //정렬 후 배열의 중앙 값 찾기
+    const midArray = Math.floor(array.length/2);
+    return newArray[midArray];
 }
 solution4([1,2,7,10,11]); //7
 solution4([9, -1, 0]); //0
