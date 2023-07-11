3a5137e6a6f979025518c200fde330fa70e23548
임하늘 <hnlim2244@gmail.com>
2023-07-11 22:26:30 +0900

    짝수홀수 개수 진행

--- 코딩테스트/코딩테스트입문/level01_0711.js
+++ 코딩테스트/코딩테스트입문/level01_0711.js
@@ -34,18 +34,44 @@ solution1(64,6);
 /**
  * 점의 위치 구하기
  */
-function solution2 (dot) {
-    let answer = 0;
-    for (let i = 0; i < dot.length; i++) {
-        answer = dot[i];
-        
-        console.log(answer, x)
-        if(0 < answer ) {
-            console.log('1사분면')
-        }
+function solution2(dot) {
+    let x = dot[0];
+    let y = dot[1];
+    if(0 < x && 0 < y) {
+        return 1;
+    } else if (0 > x && 0 < y) {
+        return 2;
+    } else if (0 > x && 0 > y) {
+        return 3;
+    } else {
+        return 4;
     }
-    
 }
 
-solution2([2, 4]); //1
-solution2([-7, 9]); //2
+// solution2([2, 4]); //1
+// solution2([-7, 9]); //2
+
+/**
+ * 짝수 홀수 개수
+ */
+function solution3 (num_list) {
+    let answer = [];
+    let odd = 0; //홀수의 개수
+    let even = 0; //짝수의 개수
+    let count = 0;
+    console.log(num_list)
+    for(let i = 0; i < num_list.length; i++) {
+        console.log(num_list[i])
+        if(num_list[i] % 2 === 0) {
+            even = count++;
+            console.log('짝수개수', even)
+        } else if (num_list[i] % 1 === 0) {
+            odd = count++;
+            console.log('홀수개수', odd)
+        }
+    }
+    // return answer;
+}
+
+solution3 ([1,2,3,4,5]); //[2,3]
+solution3 ([1,3,5,7]); //[0,4]
